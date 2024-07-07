75bd2b21d37751c0861fbb3e89a5747156837242
miguegta <miguelgaitan4@gmail.com>
2024-07-06 19:53:00 -0600

    FundamentosCs

---
+++ FundamentosCs/Seccion04/TipoDeCambio/Program.cs
@@ -0,0 +1,21 @@
+﻿//Crear variables
+double tasaCompraDolares = 0;
+double tasaVentaDolares = 0;
+
+//Obtener tasas
+Console.WriteLine("Ingresa la tasa de cambio para comprar dolares: ");
+tasaCompraDolares =  Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingresa la tasa de cambio para vender dolares: ");
+tasaVentaDolares = Convert.ToDouble(Console.ReadLine());
+
+//Comprar dolares (Cambiar de Quetzales a Dolares)
+Console.WriteLine("Ingreas cuantos dolares quieres comprar: ");
+double nDolares = Convert.ToDouble(Console.ReadLine());
+double cambioCompraDolares = nDolares * tasaVentaDolares;
+Console.WriteLine($"Comprarnos {nDolares} dolares equivale depositarnos {cambioCompraDolares} Quetzales.");
+
+//Verder dolares (Cambiar Dolares a Quetzales)
+Console.WriteLine("Ingresa cuantos dolares quieres vender: ");
+double numDolares = Convert.ToDouble(Console.ReadLine());
+double cambioVentaDolares = numDolares * tasaCompraDolares;
+Console.WriteLine($"Vendernos {numDolares} dolares equivale darte {cambioVentaDolares} Quetzales.");
---
+++ FundamentosCs/Seccion04/UsoReadLine/Program.cs
@@ -0,0 +1,15 @@
+﻿//Uso de ReadLine
+WriteLine("Introduce tu nombre: ");
+string? nombre = ReadLine();
+WriteLine($"El nombre que ingresaste es {nombre}");
+
+//Leer numero
+WriteLine("Introduce tu edad: ");
+string? edadTexto = ReadLine();
+int edad = Convert.ToInt32(edadTexto);
+WriteLine($"Mi edad es {edad}");
+
+//Posible error
+WriteLine("Introduce tu telefono");
+string? tel = ReadLine();
+WriteLine($"Mi telefono es {tel}");

d3dab27f6633ab73ff0b9657d934c3dc72a40a30
miguegta <miguelgaitan4@gmail.com>
2024-08-10 18:12:56 -0600

    Add files via upload

---
+++ FundamentosCs/Sección06/RecorrerObjetos/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+  Programa que indica si un correo es valido
+  según si cuenta con @ y .com
+ */
+
+string correo = "";
+bool tieneArroba = false;
+
+Console.WriteLine("Ingrese su correo: ");
+correo = Console.ReadLine();
+
+for (int i = 0; i < correo.Length; i++)
+{
+    if (correo[i] == '@' )
+    {
+        tieneArroba = true;
+    }
+}
+
+if (tieneArroba)
+{
+    if (correo.Contains(".com"))
+    {
+        Console.WriteLine("Correo valido!");
+    }
+    else
+    {
+        Console.WriteLine("Correo invalido, faltan caracteres");
+    }
+}
+else
+{
+    Console.WriteLine("Este correo no es valido, Ingresa un correo valido.");
+}

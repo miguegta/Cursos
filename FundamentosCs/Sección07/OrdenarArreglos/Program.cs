2dfee7816d401c03a6527ea2d24c6a3955089d9f
miguegta <miguelgaitan4@gmail.com>
2024-09-03 16:54:58 -0600

    FundamentosCs

---
+++ FundamentosCs/Sección07/OrdenarArreglos/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] numeros = { 5, 2, 6, 10, 9, 11, 15, 20 };
+
+//Orden ascendente
+Console.WriteLine("Ascendente");
+Array.Sort(numeros);
+foreach (int n in numeros)
+{
+    Console.WriteLine(n);
+}
+
+//Orden descendente
+Console.WriteLine("\nDescendente");
+Array.Sort(numeros, (x, y) => y.CompareTo(x));
+foreach (int n in numeros)
+{
+    Console.WriteLine(n);
+}
+
+//2da forma de hacer orden descendente
+Console.WriteLine("\nUsando Reverse");
+Array.Sort(numeros);    //Forma ascendente
+Array.Reverse(numeros);
+foreach (int n in numeros)
+{
+    Console.WriteLine(n);
+}

b9ea4fdeedacb39408c419a8ab8e17b37fe0c1cc
miguegta <miguelgaitan4@gmail.com>
2024-09-08 21:20:01 -0600

    FundamentosCs

---
+++ FundamentosCs/Sección08/MétodoYTuplas/Program.cs
@@ -0,0 +1,8 @@
+﻿//Tuplas / Tuples (ENG)
+(double, string) t1 = (4.50, "Precio");
+Console.WriteLine($"Tuplas con elmentos {t1.Item1} y {t1.Item2}");
+
+//Podemos especificar nombres
+(string nombre, int edad) t2 = ("Juan", 20);
+Console.WriteLine($"{t2.nombre} tiene {t2.edad} años.");
+
---
+++ FundamentosCs/Sección08/ParametrosMultiples/Program.cs
@@ -0,0 +1,12 @@
+﻿void MostrarElementos(params int[] numeros)
+{
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        Console.WriteLine(numeros[i]);
+    }
+}
+
+int[] n = { 1, 2, 3, 4, 5, 6 };
+
+MostrarElementos(n);
+MostrarElementos(6, 7, 8, 9);
